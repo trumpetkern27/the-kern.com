b9f8fdd0e027c1156088393036098239f5d994ec
Big Z <trumpetkern@gmail.com>
2025-09-12 23:29:42 -0400

    pineapple

--- obj/Debug/net9.0/the-kern.com.AssemblyInfo.cs
+++ obj/Debug/net9.0/the-kern.com.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("the-kern.com")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a23ba555f62c0b94fc5059ad8d6f07c62752346")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37c7a5ff108093f4be45b6d966c8e4843b8226cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("the-kern.com")]
 [assembly: System.Reflection.AssemblyTitleAttribute("the-kern.com")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
