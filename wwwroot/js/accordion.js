d65e87ec8d7bd4467dbc1b59410f615bd87857f7
Big Z <trumpetkern@gmail.com>
2025-09-13 00:44:47 -0400

    fix accordion

--- wwwroot/js/accordion.js
+++ wwwroot/js/accordion.js
@@ -1,13 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll(".accordion-header").forEach(header => {
         header.addEventListener("click", () => {
-            header.classList.toggle("activate");
-            let content = header.nextElementSibling;
-            if (content.style.maxHeight) {
-                content.style.maxHeight = null;
-            } else {
-                content.style.maxHeight = content.scrollHeight + "px";
-            }
+            header.classList.toggle("active");
         });
     });
 });
