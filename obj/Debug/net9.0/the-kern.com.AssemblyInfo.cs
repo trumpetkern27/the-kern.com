2b96292f99582e9f749e284298a55185f6109e1e
Big Z <trumpetkern@gmail.com>
2025-09-13 00:30:56 -0400

    projects page

--- obj/Debug/net9.0/the-kern.com.AssemblyInfo.cs
+++ obj/Debug/net9.0/the-kern.com.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("the-kern.com")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+895e218ad73c579b69892354825eb759289c20c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5fc89a5d38b7dd92a84167f2eea052a87274785")]
 [assembly: System.Reflection.AssemblyProductAttribute("the-kern.com")]
 [assembly: System.Reflection.AssemblyTitleAttribute("the-kern.com")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
